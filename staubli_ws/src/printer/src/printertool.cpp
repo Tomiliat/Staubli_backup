370a74e27a3ae526f2e39ed34a3c8db9dadb319a
Tomiliat <56246110+Tomiliat@users.noreply.github.com>
2020-04-20 22:00:27 +0200

    Add files via upload
    Printertool can read robot location and act when it is needed

--- staubli_ws/src/printer/src/printertool.cpp
+++ staubli_ws/src/printer/src/printertool.cpp
@@ -4,10 +4,14 @@
 #include <math.h>
 #include <cmath>
 #include <sstream>
+#include <vector>
 // Core ros functionality like ros::init
 #include <ros/ros.h>
+//The tf package provides an implementation of a TransformListener to help make the task of receiving transforms easier. 
+//To use the TransformListener, we need to include the tf/transform_listener.h header file.
+#include <tf/transform_listener.h>
 
-// printer variables & parameters
+// printer variables & parameters (F)
 float printspeed = 40;
 float diambuse = 0.4;      // Buse from french = Layer
 float hcouche = 0.2;
@@ -16,6 +20,30 @@ float diamfil = 1.75;
 
 // E = ((pi * diamfil^2) / (4)) * L1. 
 
+std::string getGcodeValue(std::string gcodeOperand, std::string gcodeLine) {
+    if (isdigit(gcodeLine[gcodeLine.find(gcodeOperand) + 1]) || gcodeLine[gcodeLine.find(gcodeOperand) + 1]=='-')
+    {
+        int valueStartPos = gcodeLine.find(gcodeOperand) + 1;
+        int spacePos = gcodeLine.find(" ", valueStartPos);
+        int valueLength;
+        std::string gcodeValue;
+        if (spacePos == std::string::npos)
+        {
+            // Space not found after operand, value ends at the line end
+            valueLength = gcodeLine.length() - valueStartPos;
+            gcodeValue = gcodeLine.substr(valueStartPos);
+        }
+        else
+        {
+            // After operand found space, value is between operand and " "
+            valueLength = spacePos - valueStartPos;
+            gcodeValue = gcodeLine.substr(valueStartPos, valueLength);
+        }
+        return gcodeValue;
+    }
+    return "";
+}
+
 
 int main(int argc, char** argv)
 {
@@ -23,7 +51,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "printertool");
     ros::NodeHandle nh;
 
-    // Here we calculate new parameters to F
+    // Calculate new parameters to F
     float feedrate = (60 * 4 * diambuse * hcouche * printspeed)/(std::pow(diamfil, 2) * M_PI);
     std::ostringstream ssF;
     ssF << feedrate;
@@ -34,6 +62,9 @@ int main(int argc, char** argv)
 
     // Here we define our port and baudrate, and make communication.
     mySerial serial = mySerial("/dev/ttyUSB0", 9600);
+
+    std::vector<std::string> gcodeLines = {};
+    float rememberZ = 0;
 
     // UNCOMMENT THIS, IF you want your file to be read from same directory with the script.
     /* 
@@ -66,6 +97,8 @@ int main(int argc, char** argv)
         std::string oldE = "0";
         std::string actE;
         double newE;
+        // std::string F = F;
+        // char F;
 
         // Search F and change feedrate value. Overwrite with new parameters.
         while (getline(read, line))
@@ -77,6 +110,7 @@ int main(int argc, char** argv)
                 int feedrateLength = fSpacePos - fStartPos;
                 line.replace(fStartPos, feedrateLength, feedrateString);
             }
+
 
             // Search E and change parameters
             if (isdigit(line[line.find("E") + 1]))
@@ -106,12 +140,104 @@ int main(int argc, char** argv)
             }
 
             // cout just to see what is happening, not important and you can remove this if you don't need it in the future
-            std::cout << line << "\n";
+            // std::cout << line << "\n";
 
-            // sending our gcode line by line
-            serial.Send(line);
+            gcodeLines.push_back(line);
         }
         read.close();
+
+        /*
+        Create a TransformListener object. Once the listener is created, it starts receiving tf transformations over the wire, and buffers them for up to 10 seconds. 
+        The TransformListener object should be scoped to persist otherwise it's cache will be unable to fill and almost every query will fail. A common method is to make the 
+        TransformListener object a member variable of a class.
+        */
+
+        tf::TransformListener listener;
+        ros::Rate rate(10.0);
+        int lineIndex = 0;
+        
+        // Robot start position coordinates
+        double destinationX = 0;
+        double destinationY = 0;
+        double destinationZ = 0;
+        while (destinationX==0 && destinationY==0 && destinationZ==0)
+        {
+            // Sending lines to USB port. When XYZ found, stop and wait for matching coordinates from robot.
+            if (getGcodeValue("X", gcodeLines[lineIndex])!="" && getGcodeValue("Y", gcodeLines[lineIndex])!="" && getGcodeValue("Z", gcodeLines[lineIndex])!="")
+            {
+                destinationX = std::stod(getGcodeValue("X", gcodeLines[lineIndex]));
+                destinationY = std::stod(getGcodeValue("Y", gcodeLines[lineIndex]));
+                destinationZ = std::stod(getGcodeValue("Z", gcodeLines[lineIndex]));
+            }
+            serial.Send(gcodeLines[lineIndex]);
+            lineIndex++;
+        }
+        while (nh.ok()){
+            tf::StampedTransform transform;
+            try{
+                /*
+                The waitForTransform() takes four arguments:
+                1. Wait for the transform from this frame...
+                2. ... to this frame,
+                3. at this time, and
+                4. timeout: don't wait for longer than this maximum duration.
+                So waitForTransform() will actually block until the transform between the frames becomes available (this will usually take a few milliseconds),
+                OR --if the transform does not become available-- until the timeout has been reached.
+                */
+                listener.waitForTransform("base_link","tool0", ros::Time(0), ros::Duration(10.0));
+
+                /*
+                Here, the real work is done, we query the listener for a specific transformation. Let's take a look at the four arguments:
+                1. We want the transform from frame 1 to frame 2.
+                2. The time at which we want to transform. Providing ros::Time(0) will just get us the latest available transform.
+                3. The object in which we store the resulting transform.
+                We can also use ros::Time::now(), but for real tf use cases, it is often perfectly fine to use Time(0).
+                */
+                listener.lookupTransform("base_link", "tool0", ros::Time(0), transform);
+
+                // Print location to the console
+                double x = transform.getOrigin().x();
+                double y = transform.getOrigin().y();
+                double z = transform.getOrigin().z();
+                // std::cout << "Current position: (" << x << "," << y << "," << z << ")" << std::endl;
+
+                // Checking and waiting matching values, from gcode file and robot position.
+                if ((x*1000)==destinationX && (y*1000)==destinationY && (z*1000)==destinationZ)
+                {
+                    // Send line, if it is not including XYZ straight away
+                    while (getGcodeValue("X", gcodeLines[lineIndex])=="" && getGcodeValue("Y", gcodeLines[lineIndex])=="" && getGcodeValue("Z", gcodeLines[lineIndex])=="")
+                    {
+                        serial.Send(gcodeLines[lineIndex]);
+                        lineIndex++;
+                    }
+
+                    // XYZ found, send line and save coordinate values.
+                    serial.Send(gcodeLines[lineIndex]);
+                    if (getGcodeValue("X", gcodeLines[lineIndex])!="")
+                    {
+                        destinationX = std::stod(getGcodeValue("X", gcodeLines[lineIndex]));
+                    }
+                    if (getGcodeValue("Y", gcodeLines[lineIndex])!="")
+                    {
+                        destinationY = std::stod(getGcodeValue("Y", gcodeLines[lineIndex]));
+                    }
+                    if (getGcodeValue("Z", gcodeLines[lineIndex])!="")
+                    {
+                        destinationZ = std::stod(getGcodeValue("Z", gcodeLines[lineIndex]));
+                    }
+
+                    lineIndex++;
+                }
+            }
+            catch (tf::TransformException &ex) {
+                ROS_ERROR("%s",ex.what());
+                ros::Duration(1.0).sleep();
+                
+                continue;
+            }
+
+            rate.sleep();
+        }
     }
 
     else
